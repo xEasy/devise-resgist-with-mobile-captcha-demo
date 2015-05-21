388c6c62f33e053b1ccbd1de6c5f75a467c59d70
Yijie.Z <yijo1001@gmail.com>
2015-05-21 21:07:24 +0800

    remove user email uniq index

---
+++ db/migrate/20150521130625_remove_user_email_index.rb
@@ -0,0 +1,5 @@
+class RemoveUserEmailIndex < ActiveRecord::Migration
+  def change
+    remove_index :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521121118) do
+ActiveRecord::Schema.define(version: 20150521130625) do
 
   create_table "captchas", force: true do |t|
     t.integer  "code"
@@ -36,7 +36,6 @@ ActiveRecord::Schema.define(version: 20150521121118) do
     t.integer  "mobile"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["mobile"], name: "index_users_on_mobile", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
