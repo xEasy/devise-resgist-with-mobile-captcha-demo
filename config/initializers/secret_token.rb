e622c2d75880a99a7852a33114d5130e10ed8093
Yijie.Z <yijo1001@gmail.com>
2015-05-21 18:11:14 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhoneDevise::Application.config.secret_key_base = '6c414d8c3b58bdf0103cbe06125a904da548b81f7d844a77d38899e94ecaf4b8a71e95833d57b3a7b8e652c6eb0c670504f04763df1b73b917f6361a428f9760'
